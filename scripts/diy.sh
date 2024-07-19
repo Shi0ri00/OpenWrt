d69ea291fcb5280f650df3e7c8c9461ce1fd008c
Shiori <rc0re@outlook.com>
2024-07-19 20:44:13 +0800

    Revert "update"
    This reverts commit cee75131fe699c6c234a4a455ca7608861be81a6.

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -158,7 +158,7 @@ merge_package https://github.com/rufengsuixing/luci-app-adguardhome luci-app-adg
 merge_package https://github.com/Lienol/openwrt-package openwrt-package/luci-app-filebrowser
 merge_package https://github.com/Lienol/openwrt-package openwrt-package/luci-app-ssr-mudb-server
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-speederv2
-merge_package https://github.com/morytyann/OpenWrt-mihomo OpenWrt-mihomo
+
 #添加smartdns
 #svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/smartdns package/smartdns
 #svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns package/luci-app-smartdns
